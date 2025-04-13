570b7b7eb5127499cf8f16ef976a832949747775
Prachi Ingle <prachi.ingle@utexas.edu>
2025-04-13 00:57:17 -0500

    Encode demand vectors

--- drf_utils/model.py
+++ drf_utils/model.py
@@ -48,13 +48,36 @@ class State:
                     (alpha_i == 0) if (t == 0) else (0 <= alpha_i)
                 )
 
+        self.user_demands = [
+            [
+                Real(f"demand[userId = {i}][resource = {j}]")
+                for j in range(num_resources)
+            ]
+            for i in range(num_users)
+        ]
+
+        for user_id, demand_vector in enumerate(self.user_demands):
+            for demand in demand_vector:
+                # no zero demand allowed
+                self.user_constraints.append(And(0.0 < demand, demand <= 1.0))
+
+        # TODO: fill in scaled demand_vectors based on dominant shares
+        self.normalized_demands = []
+
+        # TODO: create constraint for dominant shares
+
+        # TODO: fill in state transition constraints
+
+        # TODO: maybe not required: Add constraint that sum_{i in [n]} (alpha_i * D_i) <= R
+
 
 """
 state:
 Time-invariant: 
-    * User.demands: (n x m) ... d_i in [0,1]
+    * User.demands: (n x m) ... d_i in [0,1] and dominant share
 Change with time:
     * resource[t]: 1 --> 0 ... should be mono decreasing
     * User.Allocations[t][userId]
+    * min dominant share = min(alpha_i * D_i)
 
 """
